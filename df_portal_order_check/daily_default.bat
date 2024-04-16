ac6c8418cae8d53c4a1fe66222aefb13e4aa3f9c
AgentMajin <truongbucon@gmail.com>
2024-04-16 15:45:22 +0700

    final commit

--- df_portal_order_check/daily_default.bat
+++ df_portal_order_check/daily_default.bat
@@ -1,3 +1,2 @@
 @echo off
-"C:\Users\kris\anaconda3\python.exe" "D:\SGC\TPTS_Project\df_portal_order_check\tpts_orders_check.py" --order_store --no_order_store
-pause
+"C:\Users\Admin\anaconda3\python.exe" "D:\P.Truong\Checking_Data\TPTS_Project\df_portal_order_check\tpts_orders_check.py" --order_store --no_order_store
--- df_portal_order_check/tpts_orders_check.py
+++ df_portal_order_check/tpts_orders_check.py
@@ -18,6 +18,13 @@ parser.add_argument('--no_order_store',action='store_true', help='True to export
 parser.add_argument('--store_list', nargs='+', type=int, help='List of store to check order list on checking date' )
 args = parser.parse_args()
 
+# Log file
+def log_file(log_content):
+    today = date.today().strftime(format='%d_%m_%y')
+    with open(today + '_log_file.txt', 'a') as f:
+        f.writelines(log_content)
+        f.write('\n')
+
 # Function to download report files from Portal
 def download_report(login_page,
                     report_page,
@@ -42,7 +49,7 @@ def download_report(login_page,
         user_field = DRIVER.find_element(By.ID,"inputUserName")
         password_field = DRIVER.find_element(By.ID,"inputPassword")
     except NoSuchElementException:
-        print('Failed to Login! Cannot locate user or password field')
+        log_file('Failed to Login! Cannot locate user or password field')
         return
     user_field.send_keys('truong-vp')
     password_field.send_keys('DFp2023')
@@ -58,23 +65,24 @@ def download_report(login_page,
                                 .find_element(By.CLASS_NAME,"dt-buttons")\
                                 .find_element(By.ID,'btnExportFFS')
     except NoSuchElementException:
-        print('Fail to download report! Cannot locate download button!')
+        log_file('Fail to download report! Cannot locate download button!')
         DRIVER.quit()
         return
     download_button.click()
     while not os.path.exists(f"{download_dir}/{xls_filename}"):
         time.sleep(1)
-    print('Downloaded successfully')
+    log_file('Downloaded successfully')
     try:
         downloaded_files = glob.glob(download_dir + '\\*')
         report_file = max(downloaded_files, key=os.path.getctime)
-        file_name = report_file[24:]
-        shutil.copy(report_file, os.path.join(dir_to_save,file_name))
+        # file_name = report_file[24:]
+        shutil.copy(report_file, os.path.join(dir_to_save,xls_filename))
+        log_file(os.path.join(dir_to_save,xls_filename))
     except:
-        print('Failed to copy downloaded report to destination directory')
+        log_file('Failed to copy downloaded report to destination directory')
         DRIVER.quit()
         return
-    print('Successfully download and save report files to destination directory')
+    log_file('Successfully download and save report files to destination directory')
     DRIVER.quit()
     return
 
@@ -85,7 +93,7 @@ def convert_xls(input_path, output_path):
             app = xw.App(visible=False)
             wb = app.books.open(input_path)
         except:
-            print('Cannot open file')
+            log_file('Cannot open file')
         # Save the file as xlsx
         wb.save(output_path)
 
@@ -138,11 +146,11 @@ def get_date(check_date, ):
     if check_date == '':
         # If no specific date is provided, use tomorrow's date for file naming
         date_string = (date.today() + timedelta(1)).strftime(format='%d_%m_%Y')
-        print("Default Date: Tomorrow - Created")
+        log_file("Default Date: Tomorrow - Created")
     else:
         # Use the provided date for file naming
         date_string = check_date
-        print(f'Used input date: {check_date}')
+        log_file(f'Used input date: {check_date}')
     return date_string
 
 def main(check_date='', 
@@ -177,11 +185,11 @@ def main(check_date='',
 
     # Get the path to the downloaded XLS file
     xls_file_path = os.path.join(os.getcwd(), 'daily_xls', xls_filename)
-    print(f'xls file path: {xls_file_path}')
+    log_file(f'xls file path: {xls_file_path}')
 
     # Generate the path to the intended output with XLSX extension
     xlsx_file_path = xls_file_path.replace('xls', 'xlsx')
-    print(f'xlsx file path: {xlsx_file_path}')
+    log_file(f'xlsx file path: {xlsx_file_path}')
 
     # Check if xls file exist, and download if the check_date is blank, because Portal only save data of tomorrow's order.
     if check_date == '' and not (os.path.isfile(xls_file_path)):
@@ -190,18 +198,17 @@ def main(check_date='',
                         download_dir = str(Path.home() / "Downloads"),
                         report_page = f"https://sgcdfportal.southeastasia.cloudapp.azure.com/order/freshfoods?category=che-bien-nau-chin&ordered_day={(date.today() + timedelta(1)).strftime(format='%d/%m/%y')}&transport_method=tat-ca",
                         dir_to_save = os.path.join(os.getcwd(), 'daily_xls'))
-
+    else:
+        log_file('Report file existed')
 
     # Convert .xls file to .xlsx file
     if not convert_xls(input_path=xls_file_path, output_path=xlsx_file_path):
-        print(xlsx_file_path)
-        print(xls_file_path)
-        print('Fail to convert xls file')
+        log_file('Fail to convert xls file')
         return
     
     # If .xls file has been successfully converted, clean and perform tasks
     else:
-        print('Successfully convert xls file. Start analyzing now.')
+        log_file('Successfully convert xls file. Start analyzing now.')
         # Clean the DataFrame
         df = clean_df(pd.read_excel(xlsx_file_path))
         # Extract the date from the file path
@@ -212,18 +219,18 @@ def main(check_date='',
                 store_with_orders, store_without_orders = store_group(df)
                 # Export DataFrame of stores with orders
                 export_df(store_with_orders, date_string + '_store_có_đặt')
-                print('Successfuly perform task: order_store')
+                log_file('Successfuly perform task: order_store')
             except:
-                print('Failed to perform task: order_store')
+                log_file('Failed to perform task: order_store')
         if no_order_store:
             try:
                 # Group stores with and without orders
                 store_with_orders, store_without_orders = store_group(df)
                 # Export DataFrame of stores without orders
                 export_df(store_without_orders, date_string + '_store_không_đặt')
-                print('Successfuly perform task: no_order_store')
+                log_file('Successfuly perform task: no_order_store')
             except:
-                print('Failed to perform task: no_order_store')
+                log_file('Failed to perform task: no_order_store')
         if store_list:  # Check if store_list is not empty
             # Loop over each store in the list
             for store in store_list:
@@ -232,9 +239,9 @@ def main(check_date='',
                     order_by_store = order_list(store, df)
                     # Export DataFrame of orders for the current store
                     export_df(order_by_store, date_string + '_' + str(store))
-                    print(f"Successfully get order list of {store}")
+                    log_file(f"Successfully get order list of {store}")
                 except:
-                    print(f'Failed to get store list of {store}')
+                    log_file(f'Failed to get store list of {store}')
 
 if __name__ == "__main__":
     main(check_date=args.check_date,
